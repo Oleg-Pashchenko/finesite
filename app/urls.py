60c8eaeab310ce50e6df31447bdf3cb84b176a1b
olehandro <odpash.itmo@gmail.com>
2023-12-20 00:46:49 +0300

    Added models.py

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.test, name='home'),
-    path('finance/', views.test, name='finance'),
-    path('tasks/', views.test, name='tasks'),
-    path('targets/', views.test, name='targets'),
-    path('notes/', views.test, name='notes')
+    path('home/', views.home, name='home'),
+    path('finance/', views.finance, name='finance'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('targets/', views.targets, name='targets'),
+    path('notes/', views.notes, name='notes'),
+
+    path('add-transaction/', views.add_transaction, name='add_transaction'),
+
+    path('add-note/', views.add_note, name='add_note'),
+    path('view-note/<int:note_id>', views.add_note, name='add_note'),
+
+    path('add-target/', views.add_target, name='add_target'),
+    path('view-target/<int:target_id>', views.view_target, name='view_target'),
+
+    path('add-task/', views.add_task, name='add_task'),
+    path('view-tasks/<int:task_id>', views.view_task, name='view_task'),
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,22 @@
+from django.http import HttpRequest
+from django.shortcuts import render
 
 
-def test():
-    pass
+def home(request: HttpRequest):
+    return render(request, 'app/home/home.html')
+
+
+def finance(request: HttpRequest):
+    return render(request, 'app/home/home.html')
+
+
+def tasks(request: HttpRequest):
+    return render(request, 'app/home/home.html')
+
+
+def targets(request: HttpRequest):
+    return render(request, 'app/home/home.html')
+
+
+def notes(request: HttpRequest):
+    return render(request, 'app/home/home.html')
